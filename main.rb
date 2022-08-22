45151edbdc7bb5b3c7db6e3d0cc957198865d2cf
MT91-dev <tariqm1991@gmail.com>
2022-08-22 21:57:50 +0000

    Main file that requires all other files and runs instance of the game class

---
+++ main.rb
@@ -0,0 +1,9 @@
+require './game'
+require './player'
+require './question'
+
+# command below makes a new instance of the Game class
+game = Game.new
+
+# command below calls the start method within the game class and the game begins
+game.start
